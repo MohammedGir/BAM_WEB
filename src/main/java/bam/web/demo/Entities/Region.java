4b3b4cc3daf7ebf104b76ebb8131dd9f6296f0dd
med <eddaou.med@gmail.com>
2017-09-27 17:43:15 +0100

    v0.0.6

--- src/main/java/bam/web/demo/Entities/Region.java
+++ src/main/java/bam/web/demo/Entities/Region.java
@@ -1,9 +1,7 @@
 package bam.web.demo.Entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Region {
@@ -11,12 +9,14 @@ public class Region {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String nom;
+    private Set<Ville> villes;
 
     public Region() {
     }
 
-    public Region(String nom) {
+    public Region(String nom, Set<Ville> villes) {
         this.nom = nom;
+        this.villes = villes;
     }
 
     public String getNom() {
@@ -25,5 +25,14 @@ public class Region {
 
     public void setNom(String nom) {
         this.nom = nom;
+    }
+
+    @OneToMany(mappedBy = "region",cascade = CascadeType.ALL)
+    public Set<Ville> getVilles() {
+        return villes;
+    }
+
+    public void setVilles(Set<Ville> villes) {
+        this.villes = villes;
     }
 }
